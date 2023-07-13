f4b0ccb5df29010211a638d509e9cfba29088332
carlos8788 <carlos8788@gmail.com>
2023-07-12 21:19:33 -0300

    update class Main

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ from controlador import EstudianteControlador
 from modelo import EstudianteModelo
 from vista import EstudianteVista
 
-def main():
-    modelo = EstudianteModelo()
-    controlador = EstudianteControlador(modelo)
-    vista = EstudianteVista(controlador)
+class Main:
 
-    while True:
-        print("\n1. Agregar estudiante")
-        print("2. Mostrar estudiantes")
-        print("3. Salir")
-        opcion = int(input("Elige una opción: "))
+    @classmethod
+    def main(self):
+        self.modelo = EstudianteModelo()
+        self.controlador = EstudianteControlador(self.modelo)
+        self.vista = EstudianteVista(self.controlador)
 
-        if opcion == 1:
-            vista.agregar_estudiante()
-        elif opcion == 2:
-            vista.mostrar_estudiantes()
-        elif opcion == 3:
-            break
-        else:
-            print("Opción no válida. Por favor, vuelve a intentarlo.")
+        while True:
+            print("\n1. Agregar estudiante")
+            print("2. Mostrar estudiantes")
+            print("3. Salir")
+            opcion = int(input("Elige una opción: "))
+
+            if opcion == 1:
+                self.vista.agregar_estudiante()
+            elif opcion == 2:
+                self.vista.mostrar_estudiantes()
+            elif opcion == 3:
+                break
+            else:
+                print("Opción no válida. Por favor, vuelve a intentarlo.")
 
 if __name__ == "__main__":
-    main()
+    Main.main()
